5e2cf5867606d147cc743f44a6c80c0e8f9885d7
Guttorm R. T. Zapffe <guttormz@viken.no>
2023-01-28 00:26:07 +0100

    Added code so i dont need to delete the database before running the code

--- to databaser.py
+++ to databaser.py
@@ -8,7 +8,10 @@ cur = conn.cursor() # Lager en cursor
 def table(): # Lager funksjonen table()
 
     if cur.execute("SELECT name FROM sqlite_schema WHERE type='table' AND name='post'"): # Sjekker om tabellen postnummer eksisterer
-        cur.execute("DROP TABLE post") # Sletter tabellen postnummer
+        try:
+            cur.execute("DROP TABLE post") # Sletter tabellen postnummer
+        except:
+            sq.OperationalError
 
     cur.execute('''CREATE TABLE IF NOT EXISTS post (
                     postnummer      TEXT PRIMARY KEY, 
@@ -18,15 +21,18 @@ def table(): # Lager funksjonen table()
                     kategori TEXT NOT NULL);''') # Lager tabellen postnummer
 
     if cur.execute("SELECT name FROM sqlite_schema WHERE type='table' AND name='kunder'"): # Sjekker om tabellen kunder eksisterer
-       cur.execute("DROP TABLE kunder") # Sletter tabellen kunder
-
+        try:
+            cur.execute("DROP TABLE kunder") # Sletter tabellen kunder
+        except:
+            sq.OperationalError
+            
     cur.execute("CREATE TABLE IF NOT EXISTS kunder (\
                     Kundenr    INTEGER PRIMARY KEY AUTOINCREMENT, \
                     fnavn      TEXT    NOT NULL, \
                     enavn      TEXT    NOT NULL, \
-                    tlf        TEXT,\
-                    epost      TEXT    NOT NULL, \
-                    postnummer    TEXT, \
+                    epost      TEXT    NOT NULL,\
+                    tlf        TEXT    , \
+                    postnummer TEXT, \
                     FOREIGN    KEY (postnummer) REFERENCES postnummer (post));")
 
     conn.commit() # Lagrer endringene
@@ -48,13 +54,23 @@ def kunder(): # Lager funksjonen kunder
     f = open('randoms.csv', 'r') # Åpner filen randoms.csv i read
 
     for line in tqdm(f, total=200): # Går gjennom hver linje i filen
-        cur.execute('INSERT INTO kunder (fnavn, enavn, tlf, epost, postnummer) VALUES (?,?,?,?,?)', (line.split(','))) # Legger til hver linje i tabellen kunder
+        cur.execute('INSERT INTO kunder (fnavn, enavn, epost, tlf, postnummer) VALUES (?,?,?,?,?)', (line.split(','))) # Legger til hver linje i tabellen kunder
 
     cur.execute("DELETE FROM kunder WHERE fnavn IS 'fname'") # Sletter linja i tabellen kunder som inneholder "fname"
     print("Informasjon hentet om kunder") # Printer ut at informasjonen er hentet
 
     conn.commit() # Lagrer endringene
 
+def info(): # Lager funksjonen info
+    svar=(input("Vil du se info om kunder? (Ja/Nei): ")) # Spør brukeren om de vil se info om kunder
+    if svar == "Ja" or svar == "ja" or svar == "j" or svar == "J": # Sjekker om svaret er ja
+        find() # Kaller på funksjonen find
+
+    if svar == "Nei" or svar == "nei" or svar == "n" or svar == "N": # Sjekker om svaret er nei
+        print("Ok, da avslutter vi programmet.") # Printer ut at vi avslutter programmet
+        exit() # Avslutter programmet
+
+    conn.commit
 
 def find(): # Lager funksjonen find
     knr=int(input("Hvilken kunde vil du se info om? (Kundenr): ")) # Spør brukeren om hvilken kunde de vil se info om
@@ -66,20 +82,14 @@ def find(): # Lager funksjonen find
         print("Dette taller er for lavt, prøv et høyere nummer. (Minst 2)") # Printer ut at dette tallet er for lavt
         find() # Kaller på funksjonen find igjen
 
-    else: cur.execute("SELECT Kunder.kundenr,Kunder.fnavn, Kunder.enavn, Kunder.epost, Kunder.tlf, Kunder.postnummer,post.kommunenavn, post.kategori, post.kommunenummer, post.poststed FROM post INNER JOIN Kunder ON Kunder.[postnummer]=post.[postnummer] WHERE Kunder.kundenr = ?",(knr,))
-    print(cur.fetchall()) # Printer ut info om kundenr
+    cur.execute("SELECT post.kommunenavn, post.kategori, post.kommunenummer, post.poststed, kunder.Kundenr, kunder.fnavn, kunder.enavn, kunder.epost, kunder.tlf, kunder.postnummer FROM post INNER JOIN kunder ON kunder.[postnummer]=post.[postnummer] WHERE kunder.Kundenr = ?",(knr, ))
+    cur.fetchall() # Printer ut info om kunden
+    print(cur.fetchall()) # Printer ut info om kunden
 
     conn.commit() # Lagrer endringene
 
 
-def info(): # Lager funksjonen info
-    svar=(input("Vil du se info om kunder? (Ja/Nei): ")) # Spør brukeren om de vil se info om kunder
-    if svar == "Ja" or svar == "ja" or svar == "j" or svar == "J": # Sjekker om svaret er ja
-        find() # Kaller på funksjonen find
 
-    if svar == "Nei" or svar == "nei" or svar == "n" or svar == "N": # Sjekker om svaret er nei
-        print("Ok, da avslutter vi programmet.") # Printer ut at vi avslutter programmet
-        exit() # Avslutter programmet
 
 
 def main(): # Lager funksjonen main
